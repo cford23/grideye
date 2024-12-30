5db9fada249cb90f627b6abfd843c71de3c54330
cford23 <18637383+cford23@users.noreply.github.com>
2024-12-29 19:50:54 -0600

    added predict script

--- predict.py
+++ predict.py
@@ -1,14 +1,17 @@
 import config
+import matplotlib.pyplot as plt
 from model import Model
 import os
+from PIL import Image, ImageDraw, ImageFont
 import torch
 import utils
 
 
-image_path = os.path.join(config.DATA_DIR, 'images', 'a816fba6-image_83.png')
-coco_path = os.path.join(config.DATA_DIR, 'result.json')
+image_filename = 'f36e6c12-5ytnuhwh.png'
+image_path = os.path.join(config.DATA_DIR, 'images', image_filename)
+
 print('Image with correct boxes')
-utils.display_original_image(image_path, coco_path)
+utils.display_original_image(image_path)
 
 model = Model(num_classes=config.NUM_CATEGORIES)
 model.load_state_dict(torch.load(config.MODEL_PATH))
@@ -20,3 +23,30 @@ utils.display_objects(image_path, prediction)
 
 print('Image with rounded prediction boxes')
 utils.display_objects(image_path, prediction, round_pred=True)
+
+# TODO: Move the following into a function that can be used multiple times
+
+# Filter model object predictions
+filtered_preds = utils.filter_predictions(prediction, config.coco, verbose=False)
+
+# Display filtered objects on original image
+print('Image with filtered rounded prediction boxes')
+image = Image.open(image_path)
+draw = ImageDraw.Draw(image)
+font = ImageFont.load_default()
+
+color = 'red'
+for det_object in filtered_preds:
+    draw.rectangle(det_object['box'], outline=color, width=3)
+    category_name = utils.get_category_name(det_object['label'], config.coco)
+    score = det_object['score']
+    draw.text(
+        (det_object['box'][0], det_object['box'][1]),
+        f'Class: {category_name}, Score: {score:.2f}',
+        fill=color,
+        font=font
+    )
+
+plt.imshow(image)
+plt.axis('off')
+plt.show()
--- utils.py
+++ utils.py
@@ -1,42 +1,38 @@
 import config
 from functools import wraps
 import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 import time
 import torch
 import torchvision.transforms as T
 
 
+#################### GET IMAGE ID ####################
+def get_image_id(image_path, coco):
+    for image_id in coco.getImgIds():
+        image_info = coco.loadImgs(image_id)[0]
+        if image_info['file_name'] in image_path:
+            return image_info['id']
+    return None
+
+
 #################### GET IMAGE ANNOTATIONS ####################
-def get_image_annotations(image_filename):
-    img_ids = config.coco.getImgIds()
-    img_id = None
-    for img_id in img_ids:
-        img_info = config.coco.imgs[img_id]
-        if image_filename in img_info['file_name']:
-            break
-    else:
-        print(f"Image '{image_filename}' not found.")
-        return []
-
-    print('Image ID:', img_id)
-
-    ann_ids = config.coco.getAnnIds(imgIds=img_id)
-    annotations = config.coco.loadAnns(ann_ids)
-
-    print('Number of annotations:', len(annotations))
-
+def get_image_annotations(image_id, coco):
+    ann_ids = coco.getAnnIds(imgIds=image_id)
+    annotations = coco.loadAnns(ann_ids)
     return annotations
 
 
 #################### DISPLAY ORIGINAL IMAGE ####################
 def display_original_image(image_path):
-    image_filename = image_path.split('/')[-1]
-
     image = Image.open(image_path)
     draw = ImageDraw.Draw(image)
 
-    annotations = get_image_annotations(image_filename, config.coco)
+    image_id = get_image_id(image_path, config.coco)
+
+    annotations = get_image_annotations(image_id, config.coco)
+    print('Number of annotations:', len(annotations))
     for ann in annotations:
         x, y, width, height = ann['bbox']
         draw.rectangle([x, y, x + width, y + height], outline='red', width=3)
@@ -75,6 +71,28 @@ def detect_objects(model, image_path):
     return prediction[0]
 
 
+#################### ROUND BOX ####################
+def round_box(bbox, tile_length):
+    x1, y1, x2, y2 = bbox
+
+    def round_to_nearest(value):
+        lower_bound = (value // tile_length) * tile_length
+        upper_bound = np.ceil(value / tile_length) * tile_length
+
+        # Check which is closer
+        if abs(value - lower_bound) <= abs(value - upper_bound):
+            return int(lower_bound)  # Closer to lower bound
+        else:
+            return int(upper_bound)  # Closer to upper bound
+
+    x1_rounded = round_to_nearest(x1)
+    y1_rounded = round_to_nearest(y1)
+    x2_rounded = round_to_nearest(x2)
+    y2_rounded = round_to_nearest(y2)
+
+    return [x1_rounded, y1_rounded, x2_rounded, y2_rounded]
+
+
 #################### GET CATEGORY BY ID ####################
 def get_category_by_id(categories, category_id):
     for category in categories:
@@ -100,3 +118,90 @@ def get_box_indices(box, tile_length, num_rows, num_cols):
                                  for col in range(col_idx, col_idx + width_tiles)]
 
     return indices_in_box
+
+
+#################### DISPLAY OBJECTS ####################
+def display_objects(image_path, prediction, round_pred=False, verbose=False, draw_grid=False):
+    image = Image.open(image_path)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.load_default()
+
+    if draw_grid:
+        # Draw vertical lines
+        for x in range(0, config.IMAGE_WIDTH, config.TILE_LENGTH):
+            draw.line([(x, 0), (x, config.IMAGE_HEIGHT)], fill='black', width=1)
+
+        # Draw horizontal lines
+        for y in range(0, config.IMAGE_HEIGHT, config.TILE_LENGTH):
+            draw.line([(0, y), (config.IMAGE_WIDTH, y)], fill='black', width=1)
+
+    color = 'red'
+    for box, label, score in zip(prediction['boxes'], prediction['labels'], prediction['scores']):
+        box = [round(i.item()) for i in box.cpu()]
+        if round_pred:
+            box = round_box(box, config.TILE_LENGTH)
+        label = label.item()
+        score = score.item()
+        if verbose:
+            print('Box:', box)
+            print('Label:', label)
+            print('Score:', score)
+            print()
+
+        draw.rectangle(box, outline=color, width=3)
+        draw.text((box[0], box[1]), f'Class: {label}, Score: {score:.2f}', fill=color, font=font)
+
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
+
+
+#################### GET CATEGORY NAME ####################
+def get_category_name(category_id, coco):
+    for _, category in coco.cats.items():
+        if category['id'] == category_id:
+            return category['name']
+    print(f'Category name for category ID {category_id} not found')
+    return None
+
+
+#################### FILTER PREDICTIONS ####################
+def filter_predictions(predictions, coco, verbose=False):
+    objects = []
+    for box, label, score in zip(predictions['boxes'], predictions['labels'], predictions['scores']):
+        box = [round(i.item()) for i in box.cpu()]
+        box = round_box(box, config.TILE_LENGTH)
+        objects.append({
+            'box': box,
+            'label': label.item(),
+            'score': score.item()
+        })
+
+    # Removes any locations that have multiple objects predictions and keeps the highest scored object
+    sorted_objects = sorted(objects, key=lambda x: (x['box'], -x['score']))
+    unique_data = {}
+    for item in sorted_objects:
+        values = tuple(item['box'])  # Convert list to tuple to use as a dictionary key
+        if values not in unique_data or item['score'] > unique_data[values]['score']:
+            unique_data[values] = item
+
+    # Extract the filtered list
+    filtered_data = list(unique_data.values())
+
+    # Filter out any low scoring objects
+    SCORE_THRESHOLD = 0.8
+    filtered_data = [item for item in filtered_data if item['score'] >= SCORE_THRESHOLD]
+
+    if verbose:
+        print('Number of detected objects after filtering:', len(filtered_data))
+
+    # Iterate through remaining items and add category name
+    for item in filtered_data:
+        item['category'] = get_category_name(item['label'], coco)
+        if verbose:
+            print('Location:', item['box'])
+            print('Category:', item['category'])
+            print('Score:', item['score'])
+            print()
+
+    return filtered_data
