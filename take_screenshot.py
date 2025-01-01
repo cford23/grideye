5121ff10ec4df95222abc22fc5ba962ef37d6575
cford23 <18637383+cford23@users.noreply.github.com>
2024-12-31 22:37:52 -0600

    removed scripts directory

--- take_screenshot.py
+++ take_screenshot.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add the root directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import argparse
 import os
 import random
